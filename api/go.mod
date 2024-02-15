ac2c86581e9cd229af7c74dcc257a2b00cab2ff9
Pedro Ivo <pi3virgula14@outlook.com>
2024-02-15 01:44:33 -0300

    removing prefork

--- api/go.mod
+++ api/go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
--- api/main.go
+++ api/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/ImPedro29/rinha-backend-2024/api/routes"
 	"github.com/valyala/fasthttp"
-	"github.com/valyala/fasthttp/prefork"
 	"go.uber.org/zap"
 )
 
@@ -25,9 +24,9 @@ func main() {
 	server := &fasthttp.Server{
 		Handler: routes.InitRoutes,
 	}
-	preforkServer := prefork.New(server)
+	//preforkServer := prefork.New(server)
 
-	if err := preforkServer.ListenAndServe(":" + port); err != nil {
+	if err := server.ListenAndServe(":" + port); err != nil {
 		log.Fatalf("Error in ListenAndServe: %v", err)
 	}
 }
